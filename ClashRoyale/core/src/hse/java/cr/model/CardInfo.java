e77958e8f3e3e20d0655ba2a4866cde71ad08a68
AlexanderShvedov <5661685a@mail.ru>
2022-06-03 00:32:30 +0300

    deck maker

--- ClashRoyale/core/src/hse/java/cr/model/CardInfo.java
+++ ClashRoyale/core/src/hse/java/cr/model/CardInfo.java
@@ -1,48 +1,117 @@
 package hse.java.cr.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class CardInfo {
-    public Animation<TextureRegion> getAnimation() {
-        return animation;
+import java.util.ArrayList;
+
+public class CardInfo extends Actor {
+    private int index = 0;
+    BitmapFont font = new BitmapFont();
+    private TextureRegion curFrame;
+    private final ArrayList<Animation<TextureRegion>> animation;
+    private final ArrayList<String> typeOfCard;
+    private final ArrayList<String> name;
+    private final ArrayList<Integer> hp;
+    private final ArrayList<Integer> attack;
+    private final ArrayList<Integer> radious;
+    private final ArrayList<TextureRegion> curFrames;
+    private final ArrayList<Integer> addedCards;
+    private final int maxCardsInDeck = 6;
+    private final ArrayList<Float> frameDeltas;
+    private float frameDelta = 0f;
+
+    public CardInfo() {
+        curFrame = new Sprite();
+        animation = new ArrayList<>();
+        typeOfCard = new ArrayList<>();
+        name = new ArrayList<>();
+        hp = new ArrayList<>();
+        attack = new ArrayList<>();
+        radious = new ArrayList<>();
+        addedCards = new ArrayList<>();
+        curFrames = new ArrayList<>();
+        frameDeltas = new ArrayList<>();
+        for (int i = 0; i < maxCardsInDeck; i++) {
+            curFrames.add(null);
+            addedCards.add(null);
+            frameDeltas.add(0f);
+        }
     }
 
-    public String getTypeOfCard() {
-        return typeOfCard;
+    public void add(TextureAtlas atlas, String nameOfCard, String type, int hp1, int attack1, int radious1) {
+        Animation<TextureRegion> animation1 = new Animation<>(0.06f, atlas.getRegions());
+        animation1.setPlayMode(Animation.PlayMode.LOOP);
+        animation.add(animation1);
+        typeOfCard.add(type);
+        name.add(nameOfCard);
+        hp.add(hp1);
+        attack.add(attack1);
+        radious.add(radious1);
     }
 
-    public String getName() {
-        return name;
+    public int getIndex() {
+        return index;
     }
 
-    public int getHp() {
-        return hp;
+    public int getSize() {
+        return animation.size();
     }
 
-    public int getAttack() {
-        return attack;
+    public void setIndex(int i) {
+        index = i;
     }
 
-    public int getRadious() {
-        return radious;
+    public void addCard(int index) {
+        for (int i = 0; i < maxCardsInDeck; i++) {
+            if (addedCards.get(i) == null) {
+                addedCards.set(i, index);
+                return;
+            }
+        }
     }
 
-    private final Animation<TextureRegion> animation;
-    private final String typeOfCard;
-    private final String name;
-    private final int hp;
-    private final int attack;
-    private final int radious;
-
-    public CardInfo(TextureAtlas atlas, String name, String typeOfCard, int hp, int attack, int radious) {
-        animation = new Animation<>(0.06f, atlas.getRegions());
-        animation.setPlayMode(Animation.PlayMode.LOOP);
-        this.typeOfCard = typeOfCard;
-        this.name = name;
-        this.hp = hp;
-        this.attack = attack;
-        this.radious = radious;
+    public void deleteCard(int index) {
+        for (int i = 0; i < maxCardsInDeck; i++) {
+            if (addedCards.get(i) != null && addedCards.get(i) == index) {
+                addedCards.set(i, null);
+                return;
+            }
+        }
     }
+
+    private void drawAddedCards(Batch batch, float parentAlpha, int i) {
+        if (addedCards.get(i) != null) {
+            int currentIndex = addedCards.get(i);
+            curFrames.set(i, animation.get(currentIndex).getKeyFrame(frameDeltas.get(i)));
+            batch.draw(curFrames.get(i), 6 * Gdx.graphics.getWidth() / 10 + (i % 2) * 2 * Gdx.graphics.getWidth() / 10,  7.5f * Gdx.graphics.getHeight() / 10 - (i % 3) * 2.5f * Gdx.graphics.getHeight() / 10,
+                    2 * Gdx.graphics.getWidth() / 10, 8 * Gdx.graphics.getHeight() / 30);
+            frameDeltas.set(i, frameDeltas.get(i) + Gdx.graphics.getDeltaTime());
+        }
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        curFrame = animation.get(index).getKeyFrame(frameDelta);
+        String currentString = "";
+        currentString += "          " + name.get(index) + "\n";
+        currentString += "Type: " + typeOfCard.get(index) + "\n";
+        currentString += "Attack: " + attack.get(index) + "\n";
+        currentString += "Health: " + hp.get(index) + "\n";
+        currentString += "Radious: " + radious.get(index) + "\n";
+        font.draw(batch, currentString, Gdx.graphics.getWidth() / 10 , 8 * Gdx.graphics.getHeight() / 10);
+        batch.draw(curFrame, 0,  2 * Gdx.graphics.getHeight() / 10,
+                3 * Gdx.graphics.getWidth() / 10, 4 * Gdx.graphics.getHeight() / 10);
+        frameDelta += Gdx.graphics.getDeltaTime();
+        for (int i = 0; i < maxCardsInDeck; i++) {
+            drawAddedCards(batch, parentAlpha, i);
+        }
+    }
+
 }
--- ClashRoyale/core/src/hse/java/cr/screens/DeckScreen.java
+++ ClashRoyale/core/src/hse/java/cr/screens/DeckScreen.java
@@ -3,6 +3,7 @@ package hse.java.cr.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -21,19 +22,18 @@ import hse.java.cr.buttons.UIButton;
 import hse.java.cr.model.CardInfo;
 
 public class DeckScreen implements Screen {
-    int index = 0;
-    private TextureRegion curFrame;
     private MainScreen mainScreen;
     private SpriteBatch batch;
     private Assets assets;
     private final Starter game;
     private OrthographicCamera camera;
     private Sprite gameBackground;
-    private ArrayList<CardInfo> cardInfos;
+    private CardInfo cardInfos;
     private UIButton backButton;
     private UIButton leftButton;
     private UIButton rightButton;
     private UIButton addButton;
+    private UIButton deleteButton;
     private Stage stage;
 
     public DeckScreen(@NotNull Starter game, MainScreen screen) {
@@ -44,27 +44,36 @@ public class DeckScreen implements Screen {
     @Override
     public void show() {
         stage = new Stage();
-        curFrame = new Sprite();
         assets = game.getAssets();
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
-        cardInfos = new ArrayList<>();
-        cardInfos.add(new CardInfo(assets.get(Assets.fire), "Fire", "Spell", 0, 0, 0));
-        cardInfos.add(new CardInfo(assets.get(Assets.grayGolem), "Fire", "Character", 0, 0, 0));
-        cardInfos.add(new CardInfo(assets.get(Assets.greenGoblin), "Fire", "Character", 0, 0, 0));
-        cardInfos.add(new CardInfo(assets.get(Assets.greenGolem), "Fire", "Character", 0, 0, 0));
+        cardInfos = new CardInfo();
+        cardInfos.add(assets.get(Assets.fire), "Fire", "Spell", 0, 0, 0);
+        cardInfos.add(assets.get(Assets.grayGolem), "GrayGolem", "Character", 0, 0, 0);
+        cardInfos.add(assets.get(Assets.greenGoblin), "GreenGoblin", "Character", 0, 0, 0);
+        cardInfos.add(assets.get(Assets.greenGolem), "GreenGolem", "Character", 0, 0, 0);
+        cardInfos.add(assets.get(Assets.brownGolem), "BrownGolem", "Character", 0, 0, 0);
         gameBackground = new Sprite(assets.get(Assets.gameBackground));
         gameBackground.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         backButton = new UIButton("Play", assets);
         leftButton = new UIButton("Play", assets);
         rightButton = new UIButton("Play", assets);
-        leftButton.moveBy(100, 100);
-        rightButton.moveBy(-100, 100);
+        addButton = new UIButton("Play", assets);
+        deleteButton = new UIButton("Play", assets);
+        rightButton = new UIButton("Play", assets);
+        addButton.setBounds(3 * Gdx.graphics.getWidth() / 10, 0, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        deleteButton.setBounds( 3 * Gdx.graphics.getWidth() / 10, 2 * Gdx.graphics.getHeight() / 10, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        leftButton.setBounds(0, 0, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        rightButton.setBounds( 6 * Gdx.graphics.getWidth() / 10, 0, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        backButton.setBounds( 0, 8.7f * Gdx.graphics.getHeight() / 10, Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
 
+        stage.addActor(cardInfos);
         stage.addActor(backButton);
         stage.addActor(leftButton);
         stage.addActor(rightButton);
+        stage.addActor(addButton);
+        stage.addActor(deleteButton);
         Gdx.input.setInputProcessor(stage);
 
         stage.addListener(new ClickListener() {
@@ -82,6 +91,14 @@ public class DeckScreen implements Screen {
                     rightButton.setState(UIButton.State.PRESSED);
                     rightButton.playSound();
                 }
+                if (addButton.getState().equals(UIButton.State.HOVERED)) {
+                    addButton.setState(UIButton.State.PRESSED);
+                    addButton.playSound();
+                }
+                if (deleteButton.getState().equals(UIButton.State.HOVERED)) {
+                    deleteButton.setState(UIButton.State.PRESSED);
+                    deleteButton.playSound();
+                }
             }
         });
     }
@@ -97,27 +114,27 @@ public class DeckScreen implements Screen {
 
         gameBackground.setAlpha(0.6f);
         gameBackground.draw(batch);
-
-        curFrame = cardInfos.get(index).getAnimation().getKeyFrame(0);
-
-        batch.draw(curFrame, 200,  200,
-                100 , 100);
-
         batch.end();
-        stage.draw();
 
+        stage.draw();
         if (backButton.getState().equals(UIButton.State.PRESSED)) {
             game.setScreen(mainScreen);
         }
         if (leftButton.getState().equals(UIButton.State.PRESSED)) {
             leftButton.setState(UIButton.State.NORMAL);
-            index = (index - 1 + cardInfos.size()) % cardInfos.size();
-            curFrame = null;
+            cardInfos.setIndex((cardInfos.getIndex() - 1 + cardInfos.getSize()) % cardInfos.getSize());
         }
         if (rightButton.getState().equals(UIButton.State.PRESSED)) {
             rightButton.setState(UIButton.State.NORMAL);
-            index = (index + 1) % cardInfos.size();
-            curFrame = null;
+            cardInfos.setIndex((cardInfos.getIndex() + 1) % cardInfos.getSize());
+        }
+        if (addButton.getState().equals(UIButton.State.PRESSED)) {
+            addButton.setState(UIButton.State.NORMAL);
+            cardInfos.addCard(cardInfos.getIndex());
+        }
+        if (deleteButton.getState().equals(UIButton.State.PRESSED)) {
+            deleteButton.setState(UIButton.State.NORMAL);
+            cardInfos.deleteCard(cardInfos.getIndex());
         }
     }
 
